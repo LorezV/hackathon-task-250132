acac122555aadf98f2018c08f3f46699365818f7
Dmitriy Derkach <dima.derkach2004@gmail.com>
2021-12-12 16:35:42 +0500

    parser

--- alg/nla.js
+++ alg/nla.js
@@ -1,27 +1,67 @@
 const fs = require("fs");
+const az = require("az");
 var RussianStemmer = require('snowball-stemmer.jsx/dest/russian-stemmer.common.js').RussianStemmer;
 var stemmer = new RussianStemmer();
 
-let csv = fs.readFileSync("dataset.csv", "utf8");
+class Report {
+    constructor(themeId, themeText, messageId, messageText) {
+        this.themeId = themeId;
+        this.themeText = themeText;
+        this.messageId = messageId;
+        this.messageText = messageText; 
+    }
+}
 
-csvParser();
-
-function csvParser() {
-    let msgs = csv.split("\n");
+function parseCsvReports(filedata) {
+    let msgs = filedata.split("\n");
     msgs.shift();
-    
-    let lastReport = {}
+    let csvReports = []
+    let lastReport = new Report(0, "", 0, "");
+    let lastBeenFullReport = false;
 
     for (let i = 0; i < msgs.length; i ++) {
-        element = msgs[i];
-        let isFullReport = false;
-        let data = element.split(";");
-        console.log(data)
+        let element = msgs[i];
+        let array = element.split(";");
 
-        let object = {
-            theme_id: data[2],
-            theme_name: data[3],
-            message: data[4]
+        if (isFullReport(array)) {
+            let report = new Report(parseInt(array[2]), array[3], i, array.slice(4).join(";"))
+            lastReport = report;
+            if (lastBeenFullReport) {
+                csvReports.push(report);
+            }
+            lastBeenFullReport = true;
+        } else {
+            lastReport.messageText += element;
+            lastBeenFullReport = false;
         }
     }
+    return csvReports;
+}
+
+function grindingText(text) {
+    let data = text;
+    data = data.replace(/\d+/g, "");
+    data = data.replace(/\s[А-я]{1,2}\s/g, " ");
+    data = data.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, " ");
+    data = data.replace(/\s+/g, " ");
+    return data;
+}
+
+
+function isFullReport(array) {
+    let controlSymbol1 = array[0];
+    let controlSymbol2 = array[3];
+    if (!isNaN(controlSymbol1) || !isNaN(controlSymbol2)) return true;
+    return false;
+}
+
+let csv = fs.readFileSync("dataset.csv", "utf8");
+let reports = parseCsvReports(csv);
+reports.forEach(element => {
+    element.gMessageText = grindingText(element.messageText);
+});
+
+for (let i = 0; i < reports.length; i++) {
+    let tokens = az.Tokens(reports[i].gMessageText).done();
+    console.log("----------   " + reports[i].gMessageText)
 }
